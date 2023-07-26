ed88f38de48215a758b42b239297d994a15ccbd8
alex <alexnlarsen@gmail.com>
2023-07-26 21:00:59 +0200

    Add tests to verify expansion behavior

--- tests/test.rs
+++ tests/test.rs
@@ -65,7 +65,7 @@ super + c ; {1-2} ; { c, d }
         $(2)
 ";
     let hotkeys = load_config(cfg)?;
-    assert_eq!(hotkeys.len(), 2 * 2);
+    assert_eq!(hotkeys.len(), 4);
     let hk = &hotkeys[0];
     assert_eq!(hk.description(), "Charlie says arg 1");
     let hk = &hotkeys[3];
@@ -74,5 +74,66 @@ super + c ; {1-2} ; { c, d }
         assert_eq!(3, hk.chain.len());
         println!("{}", hk.description());
     }
+    Ok(())
+}
+
+#[test]
+fn test_circular_expansion() -> Result<()> {
+    let cfg = "
+# Test forward reference
+0 
+    echo a b # a:'hello forward $(2)' b:'forward' $(1)
+# Test backward reference
+1 
+    echo a b # a:'backward' b:'hello backward $(1)' $(2)
+# Test order of mapping expansion
+2
+    echo a b # a:'hello $(2)' b:'world $(1)' $(1)
+# Test order of mapping expansion 2
+3
+    echo a b # b:'hello $(1)' a:'world $(2)' $(1)
+# Test order of mapping expansion 3
+4
+    echo a b # a:'hello $(2)' b:'world $(1)' $(1) $(2)
+# Test index error expansion
+5
+    echo hello # $(2)
+";
+    let hotkeys = load_config(cfg)?;
+    assert_eq!(6, hotkeys.len());
+    let opt_2 = "hello world $(1)";
+    let opt_3 = "world hello world $(2)";
+    let expected = vec![
+        String::from("hello forward forward"),
+        String::from("hello backward backward"),
+        opt_2.to_string(),
+        opt_3.to_string(),
+        format!("{} {}", opt_2, opt_3),
+        String::from("$(2)"),
+    ];
+    for (hk, expected) in hotkeys.iter().zip(expected) {
+        assert_eq!(hk.description(), expected);
+        println!("Passed: {:?}", hk.chain[0].repr);
+    }
+    Ok(())
+}
+
+// It would be embarassing if the readme broke
+#[test]
+fn test_readme_example() -> Result<()> {
+    let cfg = "
+super + { space, shift + space } : {1-3}
+  bspc {desktop -f, node -d} '^{1-3}' #\
+  ^1:first ^2:second ^3:third \
+  desktop:'Switch to $(3) workspace' \
+  node:'Move node to $(3) workspace' \
+  $(1)
+";
+    let hotkeys = load_config(cfg)?;
+    assert_eq!(6, hotkeys.len());
+    assert_eq!("Switch to first workspace", hotkeys[0].description());
+    assert_eq!("Move node to first workspace", hotkeys[1].description());
+    assert_eq!("Move node to second workspace", hotkeys[3].description());
+    assert_eq!("Switch to third workspace", hotkeys[4].description());
     Ok(())
 }
