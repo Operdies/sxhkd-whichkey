068be3853f1917169c1fa7c73908844b4b06ccd3
alex <alexnlarsen@gmail.com>
2023-08-20 12:16:42 +0200

    Make socket connection logic more robust. Stop panicking

--- src/bin/rhkc.rs
+++ src/bin/rhkc.rs
@@ -8,13 +8,6 @@ use rhkd::rhkc::ipc::{
     self, BindCommand, IpcCommand, SubscribeCommand, SubscribeEventMask, UnbindCommand,
 };
 
-fn alive() -> Option<UnixStream> {
-    match UnixStream::connect(ipc::get_socket_path()) {
-        Ok(c) => Some(c),
-        _ => None,
-    }
-}
-
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
@@ -44,85 +37,96 @@ struct Subscription {
     with_reconnect: bool,
 }
 
-fn subscribe(sub: Subscription) {
-    let mut conn = connect(sub.with_reconnect);
+fn subscribe(sub: Subscription) -> Result<(), std::io::Error> {
     let cmd = IpcCommand::Subscribe(SubscribeCommand { events: sub.events });
     let bytes: Vec<u8> = cmd.into();
-    conn.write_all(&bytes).expect("Failed to send message");
+    let reconnect = || -> Result<UnixStream, std::io::Error> {
+        let mut new_conn = connect()?;
+        new_conn.write_all(&bytes)?;
+        Ok(new_conn)
+    };
+
+    let mut conn = if sub.with_reconnect {
+        loop {
+            if let Ok(c) = reconnect() {
+                break c;
+            }
+        }
+    } else {
+        reconnect()?
+    };
     let mut buf = [0; 200];
+
     loop {
         match conn.read(&mut buf) {
-            Ok(0) if !sub.with_reconnect => {
-                return;
-            }
+            Ok(0) if !sub.with_reconnect => break Ok(()),
             Ok(0) => {
-                println!("Connection broken. Trying to reconnect...");
-                std::thread::sleep(Duration::from_secs(1));
-                if let Some(a) = alive() {
-                    conn = a;
-                    conn.write_all(&bytes).expect("Failed to send message");
+                println!("Connection broken.");
+                if let Ok(new) = reconnect() {
                     println!("Reconnected!");
-                } else {
-                    println!("Failed to reconnect..");
+                    conn = new;
                 }
             }
             Ok(n) => print!("{}", String::from_utf8_lossy(&buf[0..n])),
             Err(e) => {
                 eprintln!("Connection broken: {} {}", e, e.kind());
                 if sub.with_reconnect {
-                    continue;
+                    println!("Connection broken.");
+                    if let Ok(new) = reconnect() {
+                        println!("Reconnected!");
+                        conn = new;
+                    }
                 } else {
-                    break;
+                    break Ok(());
                 }
             }
         }
     }
 }
 
-fn connect(retry: bool) -> UnixStream {
-    if retry {
-        loop {
-            match UnixStream::connect(ipc::get_socket_path()) {
-                Ok(s) => break s,
-                Err(e) => {
-                    eprintln!("Failed to connect to server: {}", e);
-                    std::thread::sleep(Duration::from_secs(2));
-                }
-            }
+fn connect() -> Result<UnixStream, std::io::Error> {
+    let wait_ms = [10, 25, 50, 100, 125, 150, 200, 300, 400, 500];
+    for (i, ms) in wait_ms.iter().enumerate() {
+        if let Ok(stream) = UnixStream::connect(ipc::get_socket_path()) {
+            return Ok(stream);
         }
-    } else {
-        UnixStream::connect(ipc::get_socket_path()).expect("Failed to connect to server.")
+        eprint!("\rFailed to connect to server. ({}/{})", i + 1, wait_ms.len());
+        std::thread::sleep(Duration::from_millis(*ms));
     }
+    eprintln!();
+    UnixStream::connect(ipc::get_socket_path())
 }
 
-fn bind(b: BindCommand, quiet: bool) {
-    let mut conn = connect(false);
+fn bind(b: BindCommand, quiet: bool) -> Result<(), std::io::Error> {
+    let mut conn = connect()?;
     let b = IpcCommand::Bind(b);
     let bytes: Vec<u8> = b.into();
-    conn.write_all(&bytes).expect("Failed to send message.");
+    conn.write_all(&bytes)?;
     if !quiet {
-        let _ = std::io::copy(&mut conn, &mut std::io::stdout());
+        std::io::copy(&mut conn, &mut std::io::stdout())?;
     }
+    Ok(())
 }
 
-fn unbind(u: UnbindCommand, quiet: bool) {
-    let mut conn = connect(false);
+fn unbind(u: UnbindCommand, quiet: bool) -> Result<(), std::io::Error> {
+    let mut conn = connect()?;
     let u = IpcCommand::Unbind(u);
     let bytes: Vec<u8> = u.into();
-    conn.write_all(&bytes).expect("Failed to send message.");
+    conn.write_all(&bytes)?;
     if !quiet {
-        let _ = std::io::copy(&mut conn, &mut std::io::stdout());
+        std::io::copy(&mut conn, &mut std::io::stdout())?;
     }
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let cli = Cli::parse();
     match cli.command {
         Commands::Subscribe(mut s) => {
             if s.events.is_empty() {
                 s.events.push(SubscribeEventMask::All);
             }
-            subscribe(s);
+            subscribe(s)
         }
         Commands::Bind(b) => bind(b, cli.quiet),
         Commands::Unbind(c) => unbind(c, cli.quiet),
